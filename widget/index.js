35109a20f11e6a877f1cebbbd90b19d6526ab515
propbreakerfpv <jonasebreen@gmail.com>
2023-01-20 13:58:11 -0500

    opening and closing working for button and widget. still needs styling/connection to backend

--- widget/index.js
+++ widget/index.js
@@ -1,13 +1,11 @@
-
-
-var IS_OPEN = false;
+var KARMABOX_IS_OPEN = false;
 const popupContent = `
         <div id="karmaboxid-popup-contaner">
-    <h3>donate to the<br>rob charity</h3>
+            <h3>donate to the<br>rob charity</h3>
             <div id="karmaboxid-mul-container">
                 <p class="karmaboxclass-karmapoint" style="display: inline">♥️ $3 each</p>
                 <input id="karmaboxid-karma-amount" value="1" type="text"/>
-                <button class="karmaboxclass-mul-button" id="karmaboxid-button-1">1</button>
+                <button class="karmaboxclass-mul-button" id="karmaboxid-mul-button-solected">1</button>
                 <button class="karmaboxclass-mul-button" id="karmaboxid-button-2">3</button>
                 <button class="karmaboxclass-mul-button" id="karmaboxid-button-3">5</button>
             </div>
@@ -19,8 +17,8 @@ const popupContent = `
     `;
 
 
-let payload = document.createElement("div");
-payload.innerHTML = 
+let karmabox_payload = document.createElement("div");
+karmabox_payload.innerHTML = 
     `
     <style>
 
@@ -38,6 +36,14 @@ payload.innerHTML =
         width: 50px;
         height: 30px;
     }
+    #karmaboxid-mul-button-solected {
+        border: solid yelow 2px;
+        background-color: red;
+        border-radius: 20px;
+        padding: 5px;
+        width: 30px;
+        height: 30px;
+    }
     .karmaboxclass-mul-button {
         border: solid purple 2px;
         background-color:  #ff00ff19;
@@ -51,6 +57,8 @@ payload.innerHTML =
         bottom: 0;
         right: 0;
         width: 300px;
+        margin-right: 50px;
+        margin-bottom: 50px;
     }
     #karmaboxid-popup-contaner {
         width: 300px;
@@ -60,8 +68,8 @@ payload.innerHTML =
         flex-direction: column;
         bottom: 0;
         right: 0;
-        margin-bottom: 25px;
-        margin-right: 65px;
+        margin-bottom: 75px;
+        margin-right: 75px;
         background-color: gray;
         align-items: center;
     }
@@ -77,50 +85,71 @@ payload.innerHTML =
         flex-direction: column;
         width: 300px;
     }
-    #karmaboxid-button {
-        position: absolute;
-        bottom: 0;
-        right: 0;
-    }
     .karmaboxid-form-item {
         display: block;
         padding: 5px;
         margin: 10px
 
     }
+    #karmaboxid-button {
+        width: 75px;
+        height: 75px;
+        border-radius: 50px;
+        position: absolute;
+        bottom: 0;
+        right: 0;
+        background-color: purple;
+        background-image: url(./assets/logo3.png)
+        background-size: cover;
+        border: none;
+    
+    #karmaboxid-logo {
+        background-color: red;
+        width: 1000px;
+        height: 1000px;
+    }
     </style>
     <div id="karmaboxid-wrapper">
-    <button id="karmaboxid-button">click me</button>
+    <button id="karmaboxid-button">
+    </button>
     </div>
     `;
 
 
 
-let body = document.getElementsByTagName("body")[0];
-inject(body, payload);
-body.addEventListener("click", e => {
-    let container = document.getElementById("karmaboxid-wrapper");
-    let popup = document.getElementById("karmaboxid-popup-wrapper")
-    container.removeChild(popup);
-    IS_OPEN = false;
-    console.log("testing")
+
+let karmabox_body = document.getElementsByTagName("body")[0];
+inject(karmabox_body, karmabox_payload);
+karmabox_body.addEventListener("click", _ => {
+    if (KARMABOX_IS_OPEN) {
+        let container = document.getElementById("karmaboxid-wrapper");
+        let popup = document.getElementById("karmaboxid-popup-wrapper")
+        container.removeChild(popup);
+        KARMABOX_IS_OPEN = false;
+    }
 });
 
 document.getElementById("karmaboxid-button").addEventListener("click", e => {
-    e.stopPropagation();
-    if (!IS_OPEN) {
+    try {
+        if (!KARMABOX_IS_OPEN2){
+            e.stopPropagation();
+        }
+    }catch(_){
+        e.stopPropagation();
+    }
+    if (!KARMABOX_IS_OPEN) {
         let popup = document.createElement("div");
         popup.innerHTML = popupContent;
         popup.id = "karmaboxid-popup-wrapper"
         let container = document.getElementById("karmaboxid-wrapper");
         container.insertBefore(popup, container.firstChild);
         addFormEvents();
-        IS_OPEN = true;
+        KARMABOX_IS_OPEN = true;
     } else {
         let container = document.getElementById("karmaboxid-wrapper");
         let popup = document.getElementById("karmaboxid-popup-wrapper")
         container.removeChild(popup);
-        IS_OPEN = false;
+        KARMABOX_IS_OPEN = false;
     }
 });
 
@@ -134,20 +163,29 @@ function addFormEvents() {
         e.stopPropagation();
     });
 
-    document.querySelector("#karmaboxid-button-1").addEventListener("click", e => {
+    document.querySelector("#karmaboxid-mul-button-solected").addEventListener("click", e => {
         e.preventDefault();
         let input = document.getElementById("karmaboxid-karma-amount");
         input.value = 1;
+        e.target.nextElementSibling.id = "";
+        e.target.nextElementSibling.nextElementSibling.id = "";
+        e.target.id = "karmaboxid-mul-button-solected"
     });
     document.querySelector("#karmaboxid-button-2").addEventListener("click", e => {
         e.preventDefault();
         let input = document.getElementById("karmaboxid-karma-amount");
         input.value = 3;
+        e.target.id = "karmaboxid-mul-button-solected"
+        e.target.nextElementSibling.id = "";
+        e.target.previousElementSibling.id = "";
     });
     document.querySelector("#karmaboxid-button-3").addEventListener("click", e => {
         e.preventDefault();
         let input = document.getElementById("karmaboxid-karma-amount");
         input.value = 5;
+        e.target.id = "karmaboxid-mul-button-solected"
+        e.target.previousElementSibling.id = "";
+        e.target.previousElementSibling.previousElementSibling.id = "";
     });
 }
 
@@ -155,5 +193,4 @@ function addFormEvents() {
 function inject(target, payload) {
     target.appendChild(payload);
 }
-
 
