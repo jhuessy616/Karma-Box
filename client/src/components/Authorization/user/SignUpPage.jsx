9d8845755e44fff20065b457be1b9e30201ceba7
Jamese <[jamesmodic7@gmail.coms]>
2023-01-25 15:33:57 -0500

    James

--- client/src/components/Authorization/user/SignUpPage.jsx
+++ client/src/components/Authorization/user/SignUpPage.jsx
@@ -12,18 +12,17 @@ const SignUpPage = (props) => {
     <div className="Background">
       <Navbar></Navbar>
       <Container>
-
+      <h1 className="txtcenter">Begin earning your good karma today!</h1>
+        <h2 className="txtcenter" >Sign up to be a donor!</h2>
       <Row>
 					<Col lg="4.5" md="4" xs="2"></Col>
-      <div>
-        <h1 className="txtcenter">Begin earning your good karma today!</h1>
-        <h2 className="txtcenter" >Sign up to be a donor!</h2>
-        <Row>
-          <Col lg="3" md="4" xs="8"></Col>
+      
+          <Col lg="3" md="4" xs="8">
           
 
             <DonorSignUp updateToken={props.updateToken} />
-          </div>
+            </Col>
+          
           <Col lg="4.5" md="4" xs="2"></Col>
         </Row>
       </Container>
